f1767d7855cc79f74273b447ebaee4098563c66f
dmitryofitserov <dmitryofitserov@gmail.com>
2023-07-18 13:18:15 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,8 +1,10 @@
-function skillMember() {
-  var member = {
-    name: "John",
-    age: 30,
-    address: "New York"
-  };
-  return member;
+function skillsMember() {
+    let skills = ["HTML", "CSS", "JavaScript", "Python", "Java", "C++"];
+    let skillsList = document.getElementById("skills");
+    let skillCount = skills.length;
+    let skillString = "";
+    for (let i = 0; i < skillCount; i++) {
+        skillString += `<li class="list-group-item">${skills[i]}</li>`;
+    }
+    skillsList.innerHTML = skillString;
 }
